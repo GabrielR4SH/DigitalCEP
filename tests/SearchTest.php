ffb2f6b96890835fa3dba008720dbca3b35506bb
GabrielR4SH <bieelsantana23@hotmail.com>
2025-01-14 23:13:39 -0300

    teste done

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -29,5 +29,43 @@ class SearchTest extends TestCase
         $this->assertEquals($esperado, $result); 
     }
 
-    
+    /**
+     * @dataProvider dadosTeste
+    */
+
+    public function dadosTeste()
+    {
+        return [
+            "Endereço Praça da Sé" => [
+                "01001000",
+                [
+                    "cep" => "01001-000",
+                    "logradouro" => "Praça da Sé",
+                    "complemento" => "lado ímpar",
+                    "bairro" => "Sé",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "ibge" => "3550308",
+                    "gia" => "1004",
+                    "ddd" => "11",
+                    "siafi" => "7107"
+                ]
+            ],
+            "Outro endereço (exemplo fictício)" => [
+                "05822010",
+                [
+                    "cep" => "05822-010",
+                    "logradouro" => "Rua Antônio Ramos Rosa",
+                    "complemento" => "",
+                    "bairro" => "Parque Santo Antônio",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "ibge" => "3550308",
+                    "gia" => "1004",
+                    "ddd" => "11",
+                    "siafi" => "7107"
+                ]
+            ]
+        ];
+    }
 }
